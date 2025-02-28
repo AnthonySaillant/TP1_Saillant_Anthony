495f61b549d1cec0a7efe171bbcac9d17fdffe1e
AnthonySaillant <2354779@csfoy.ca>
2025-02-28 09:34:50 -0500

    Creation et Configuration des ressources
    configuration des ressources avec les id + timestamps (pas sur) et creation des controllers (vide)

---
+++ tp1/app/Http/Resources/ActorResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ActorResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'last_name' => $this->last_name,
+            'first_name' => $this->first_name,
+            'birthdate' => $this->birthdate,
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString()
+        ];
+    }
+}
